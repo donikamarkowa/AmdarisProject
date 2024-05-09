30472c786e34b8fd6fdbdb2f73e08f6d1fea996c
donikamarkowa <donimarkova@gmail.com>
2024-05-09 13:42:46 +0300

    Implement generic repository

---
+++ WorkoutReservations.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,93 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace WorkoutReservations.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity, TContext>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected readonly TContext _dbContext;
+        protected readonly DbSet<TEntity> _set;
+
+        public GenericRepository(TContext context)
+        {
+            this._dbContext = context;
+            _set = context.Set<TEntity>();
+        }
+        public async Task Add(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await _set.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _set.Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _set.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllWithInclude(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = IncludeProperties(includeProperties);
+            return await query.ToListAsync();
+        }
+        public Task<TEntity> GetByWithInclude(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = IncludeProperties(includeProperties).Where(predicate);
+            return query.FirstOrDefaultAsync()!;
+        }
+
+
+        public async Task<IEnumerable<TEntity>> GetAllBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _set.Where(predicate).ToListAsync();
+        }
+        public async Task<TEntity> GetById(Guid id)
+        {
+            return await _set.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _set.Update(entity);
+        }
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _set.AnyAsync(predicate);
+        }
+
+
+        #region private methods
+        private IQueryable<TEntity> IncludeProperties(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> entities = _dbContext.Set<TEntity>();
+            foreach (var includeProperty in includeProperties)
+            {
+                entities = entities.Include(includeProperty);
+            }
+            return entities;
+        }
+        #endregion
+    }
+
+}
---
+++ WorkoutReservations.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace WorkoutReservations.Infrastructure.Repositories
+{
+    public interface IGenericRepository<TEntity, TContext>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        Task<TEntity> GetById(Guid id);
+        Task<TEntity> GetByWithInclude(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<IEnumerable<TEntity>> GetAllBy(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+
+}
