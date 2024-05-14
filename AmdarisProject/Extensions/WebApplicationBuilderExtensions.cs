becbe7367282bc343a3c7d999cf5f2c43396f5e0
donikamarkowa <donimarkova@gmail.com>
2024-05-14 15:25:46 +0300

    Add extensions for register authentication and add swagger

---
+++ AmdarisProject/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using WorkoutReservations.Domain.Entities;
+using WorkoutReservations.Infrastructure.Database;
+using WorkoutReservations.Infrastructure.Options;
+
+namespace AmdarisProject.Extensions
+{
+    public static class WebApplicationBuilderExtensions 
+    {
+        public static WebApplicationBuilder RegisterAuthentication(this WebApplicationBuilder builder)
+        {
+            var jwtSettings = new JwtSettings();
+            builder.Configuration.Bind(nameof(jwtSettings), jwtSettings);
+
+            var jwtSection = builder.Configuration.GetSection(nameof(jwtSettings));
+            builder.Services.Configure<JwtSettings>(jwtSection);
+
+            builder.Services
+                .AddAuthentication(a =>
+                {
+                    a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    a.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(jwt =>
+                {
+                    jwt.SaveToken = true;
+                    jwt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SigningKey ?? throw new InvalidOperationException())),
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidateAudience = true,
+                        ValidAudiences = jwtSettings.Audiences,
+                        RequireExpirationTime = false,
+                        ValidateLifetime = true
+                    };
+
+                    jwt.Audience = jwtSettings.Audiences?[0];
+                    jwt.ClaimsIssuer = jwtSettings.Issuer;
+                });
+
+            builder.Services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoles<Role>()
+            .AddSignInManager()
+            .AddEntityFrameworkStores<WorkoutReservationsDbContext>();
+
+            return builder;
+        }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Amdaris Project", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            return services;
+        }
+    }
+}
