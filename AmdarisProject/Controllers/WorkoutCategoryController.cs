56172e9e6a04a91d3cdb0262d8d4590dfec08f63
donikamarkowa <donimarkova@gmail.com>
2024-05-15 15:56:42 +0300

    Create WorkoutCategoryController method Add

---
+++ AmdarisProject/Controllers/WorkoutCategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutReservations.Application.DTOs.WorkoutCategory;
+using WorkoutReservations.Application.Services.Interfaces;
+
+namespace AmdarisProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class WorkoutCategoryController : ControllerBase
+    {
+        private readonly IWorkoutCategoryService _workoutCategoryService;
+        public WorkoutCategoryController(IWorkoutCategoryService workoutCategoryService)
+        {
+            _workoutCategoryService = workoutCategoryService;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] AddWorkoutCategoryDto dto)
+        {
+            try
+            {
+                await _workoutCategoryService.AddCategoryAsync(dto);
+
+                return Ok("Category added successfully.");
+
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                // 403 Forbidden if the user is not authorized
+                return StatusCode(403, ex.Message);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Failed to add workout.");
+            }
+        }
+    }
+}
