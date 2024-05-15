9456efe4b81927201049b2373948c998d2b5357d
donikamarkowa <donimarkova@gmail.com>
2024-05-15 17:17:02 +0300

    Update

---
+++ WorkoutReservations.Application/DTOs/Workout/WorkoutDto.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutReservations.Application.DTOs.Workout
+{
+    public class WorkoutDto
+    {
+        public string Id { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string EquipmentNeeded { get; set; } = null!;
+        public string Duration { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public int IntensityLevel { get; set; } 
+        public string Status { get; set; } = null!;
+        public string Picture { get; set; } = null!;
+        public string Price { get; set; } = null!;
+        public string RecommendedFrequency { get; set; } = null!;
+        public string WorkoutCategoryId { get; set; } = null!;
+    }
+}
