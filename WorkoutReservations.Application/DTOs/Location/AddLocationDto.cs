67800c9f4f146aacf31f0b22807811ad8fbd1d07
donikamarkowa <donimarkova@gmail.com>
2024-05-15 17:16:05 +0300

    Create new dto for add location

---
+++ WorkoutReservations.Application/DTOs/Location/AddLocationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkoutReservations.Application.DTOs.Location
+{
+    public class AddLocationDto
+    {
+        public string Id { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Latitude { get; set; } = null!;
+        public string Longitude { get; set; } = null!;
+        public string ZipCode { get; set; } = null!;
+        public int MaxCapacity { get; set; }
+    }
+}
