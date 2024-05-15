54746ed7a944ba1c842c06470c93e5af52a382bb
donikamarkowa <donimarkova@gmail.com>
2024-05-15 15:57:19 +0300

    Create dto for adding category

---
+++ WorkoutReservations.Application/DTOs/WorkoutCategory/AddWorkoutCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutReservations.Application.DTOs.WorkoutCategory
+{
+    public class AddWorkoutCategoryDto
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;       
+    }
+}
