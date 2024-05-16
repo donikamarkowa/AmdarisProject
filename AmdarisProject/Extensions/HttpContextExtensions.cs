276abe14ec8fed2cf65aa5f7a019844c4f8329fe
donikamarkowa <donimarkova@gmail.com>
2024-05-16 16:26:31 +0300

    Add new extension for getting user's id

---
+++ AmdarisProject/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace AmdarisProject.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetUserIdExtension(this HttpContext context)
+        {
+            return context.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
