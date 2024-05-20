29b5ef071436fdfdf92391e7d6a7f57f0d46369a
donikamarkowa <donimarkova@gmail.com>
2024-05-20 11:12:05 +0300

    Add dtos for adding schedule and editing category

---
+++ WorkoutReservations.Application/DTOs/Schedule/AddScheduleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutReservations.Application.DTOs.Schedule
+{
+    public class AddScheduleDto
+    {
+        public string Id { get; set; } = null!;
+        public string Date { get; set; } = null!;
+        public int Capacity { get; set; } 
+    }
+}
---
+++ WorkoutReservations.Application/DTOs/WorkoutCategory/EditWorkoutCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkoutReservations.Application.DTOs.WorkoutCategory
+{
+    public class EditWorkoutCategoryDto
+    {
+        public string Name { get; set; } = null!;
+    } 
+}
