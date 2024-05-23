be3d93848d1bf3294b6dee0cd9e4d36c1651d2bc
donikamarkowa <donimarkova@gmail.com>
2024-05-23 10:28:51 +0300

    Add EditProfileDto

---
+++ WorkoutReservations.Application/DTOs/Auth/EditProfileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutReservations.Application.DTOs.Auth
+{
+    public class EditProfileDto
+    {
+        public int? Age { get; set; }
+        public string? Bio { get; set; }
+        public double? Weight { get; set; }
+        public double? Height { get; set; }
+        public string? PhoneNumber { get; set; } 
+        public string? Picture { get; set; }
+    }
+}
