4c06d97bf77d030f93f18597db49d7f1a10e1c2e
donikamarkowa <donimarkova@gmail.com>
2024-05-23 09:58:52 +0300

    Add new TrainerDto

---
+++ WorkoutReservations.Application/DTOs/Trainer/TrainerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutReservations.Application.DTOs.Trainer
+{
+    public class TrainerDto
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
