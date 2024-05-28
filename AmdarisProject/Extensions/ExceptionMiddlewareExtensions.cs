77efc8a54d484947d485ac33c73cb0346aa9139f
donikamarkowa <donimarkova@gmail.com>
2024-05-28 16:43:58 +0300

    Add exception middleware extension with custom error

---
+++ AmdarisProject/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using AmdarisProject.Middleware;
+
+namespace AmdarisProject.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ AmdarisProject/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using AmdarisProject.Contracts;
+using System.Net;
+
+namespace AmdarisProject.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;   
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                //TODO: Add problem Details & Custom exceptions
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                var error = new Error
+                {
+                    StatusCode = context.Response.StatusCode.ToString(),
+                    Message = e.Message
+                };
+
+                await context.Response.WriteAsync(error.ToString());
+            }
+        }
+    }
+}
