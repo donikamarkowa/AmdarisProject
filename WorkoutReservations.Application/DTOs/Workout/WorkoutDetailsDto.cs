39337e5e949ee924c2fb9f7c4c57d51895cc2b1e
donikamarkowa <donimarkova@gmail.com>
2024-05-31 14:45:19 +0300

    Change status to be strng

--- WorkoutReservations.Application/DTOs/Workout/WorkoutDetailsDto.cs
+++ WorkoutReservations.Application/DTOs/Workout/WorkoutDetailsDto.cs
@@ -11,7 +11,7 @@ namespace WorkoutReservations.Application.Models.Workout
         public string Duration { get; set; } = null!;
         public string Gender { get; set; } = null!;
         public int IntensityLevel { get; set; }
-        public WorkoutStatus Status { get; set; } 
+        public string Status { get; set; } = null!;
         public string Picture { get; set; } = null!;
         public string Price { get; set; } = null!;
         public string WorkoutCategory { get; set; } = null!;
