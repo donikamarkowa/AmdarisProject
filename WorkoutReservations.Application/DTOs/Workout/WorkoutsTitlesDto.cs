0c8acf7882cb9064ce4be223195170cf164c6a64
donikamarkowa <donimarkova@gmail.com>
2024-06-10 17:16:33 +0300

    Add new dto for workout's title

---
+++ WorkoutReservations.Application/DTOs/Workout/WorkoutsTitlesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutReservations.Application.DTOs.Workout
+{
+    public class WorkoutsTitlesDto
+    {
+        public string Id { get; set; } = null!;
+        public string Title { get; set; } = null!;
+    }
+}
