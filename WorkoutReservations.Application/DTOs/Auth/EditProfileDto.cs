86f7ec2a960ef1b982f38553acae31dbf188f173
donikamarkowa <donimarkova@gmail.com>
2024-06-12 18:16:28 +0300

    Remove null! from Gender

--- WorkoutReservations.Application/DTOs/Auth/EditProfileDto.cs
+++ WorkoutReservations.Application/DTOs/Auth/EditProfileDto.cs
@@ -4,7 +4,7 @@
     {
         public int? Age { get; set; }
         public string? Bio { get; set; }
-        public string? Gender { get; set; } = null!;
+        public string? Gender { get; set; } 
         public double? Weight { get; set; }
         public double? Height { get; set; }
         public string? PhoneNumber { get; set; } 
