093d327cf25584557f4ae67f747b8cd6b65ac396
donikamarkowa <donimarkova@gmail.com>
2024-06-16 17:43:16 +0300

    Add new dto for user details

---
+++ WorkoutReservations.Application/DTOs/Auth/UserDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutReservations.Application.DTOs.Auth
+{
+    public class UserDetails
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
